cf0540534beeb231bbc8a8acb98cca352ed5be70
zjhmale <zjhmale@gmail.com>
2016-06-13 16:19:53 +0800

    refactor

--- src/Build.hs
+++ src/Build.hs
@@ -11,25 +11,25 @@ build = shakeArgs shakeOptions{shakeFiles="_build"} $ do
   want ["_build/bin/test"]
 
   phony "clean" $ do
-      putNormal "Cleaning files in _build"
-      removeFilesAfter "_build" ["//*"]
+    putNormal "Cleaning files in _build"
+    removeFilesAfter "_build" ["//*"]
 
   phony "run" $ do
-      cmd "_build/bin/main"
+    cmd "_build/bin/main"
 
   phony "test" $ do
-      cmd "_build/bin/test"
+    cmd "_build/bin/test"
 
   "_build/bin/main" %> \_ -> do
-      cs <- getDirectoryFiles "" mlFiles
-      need cs
-      () <- cmd "ocamlbuild -use-ocamlfind main.native"
-      () <- cmd "mkdir -p _build/bin"
-      cmd "mv main.native _build/bin/main"
+    cs <- getDirectoryFiles "" mlFiles
+    need cs
+    () <- cmd "ocamlbuild -use-ocamlfind main.native"
+    () <- cmd "mkdir -p _build/bin"
+    cmd "mv main.native _build/bin/main"
 
   "_build/bin/test" %> \_ -> do
-      cs <- getDirectoryFiles "" mlFiles
-      need cs
-      () <- cmd "ocamlbuild -use-ocamlfind -package oUnit test.native"
-      () <- cmd "mkdir -p _build/bin"
-      cmd "mv test.native _build/bin/test"
+    cs <- getDirectoryFiles "" mlFiles
+    need cs
+    () <- cmd "ocamlbuild -use-ocamlfind -package oUnit test.native"
+    () <- cmd "mkdir -p _build/bin"
+    cmd "mv test.native _build/bin/test"
